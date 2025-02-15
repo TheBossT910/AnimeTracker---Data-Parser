0d7232e42af5ce54f89c383269ee500157583e97
Taha Rashid <taharashid710192@gmail.com>
2025-02-15 18:10:37 -0500

    chore: created new file to parse raw AniList data to useable format

---
+++ parse_data.py
@@ -0,0 +1,10 @@
+# Taha Rashid
+# February 15, 2025
+# Description: Parses the data from AniList API to make it database-compatible
+# NOTE: Base this off of previously built fetch_anime_data/py in "Old" folder
+
+# PARSING
+# getNewlyAdded specifc parse,
+# getAiringSchedule specific parse (this would work for both weekly fetch and daily fetch)
+
+# FETCH FB DATA?
