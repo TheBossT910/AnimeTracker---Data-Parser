6ed8699cf80ffee09cdbbe7e269dd828ed9296b0
Taha Rashid <taharashid710192@gmail.com>
2025-02-15 11:12:43 -0500

    chore: created a class and added a TODO list with notes

--- fetch_data.py
+++ fetch_data.py
@@ -1,3 +1,16 @@
 # Taha Rashid
 # February 15, 2025
 # Description: Fetchs new/updated anime data via AniList API using GraphQL
+
+import requests
+
+class FetchData:
+    
+    # TODO:
+    # All of these functions will be class methods, meaning that we don't need to create objects
+    # getNewlyAdded (bi-weekly basis, gets newly added shows)
+    # getCurrentlyAiring (weekly basis, grabs the currently airing shows and their next air time, and the predicted air time after that (we grab 2 air times?))
+    # checkAiringToday (daily basis, checks if the info for shows airing today has changed)
+    # updateNextAir (daily basis, updates the next air date for shows that aired the previous day. Runs before checkAiringToday)
+    # cleanOldData (weekly basis, deletes show air data that is >2 weeks old)
+    
